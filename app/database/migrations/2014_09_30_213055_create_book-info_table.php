2ccaf6d202b00452d031d7edd3c69d0266a7d1eb
Rafi Strauss <rafikis75@gmail.com>
2014-10-01 09:28:40 -0400

    Set debug mode to true Connect to database Migrations to create initial table structure

---
+++ app/database/migrations/2014_09_30_213055_create_book-info_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookInfoTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('book-info', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('ISBN')->unique();
+			$table->string('picture_link');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('book-info');
+	}
+
+}
