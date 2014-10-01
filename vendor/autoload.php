63b22fccbe7e6d8839b3deaf6f70fc687e9bbfc9
Rafi Strauss <rafikis75@gmail.com>
2014-10-01 09:39:44 -0400

    Git, project, and other generated files

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit1e149dc94d566fbc58c100858d2dc557::getLoader();
+return ComposerAutoloaderInit417d082d3a2c94fb1125b16c9ba8c8e9::getLoader();
